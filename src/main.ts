8ca66a9a8090a957030e2b7b0062c895a59549f5
Philipp Wahle <info@wahlemedia.de>
2023-06-14 18:56:18 +0200

    feat: add vue-router

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,12 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
+import { createRouter, createWebHashHistory } from "vue-router";
+import routes from "./routes";
 
-createApp(App).mount('#app')
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes,
+});
+
+createApp(App).use(router).mount("#app");
---
+++ src/routes.ts
@@ -0,0 +1,6 @@
+import { RouteRecordRaw } from "vue-router";
+import HomePage from "./pages/HomePage.vue";
+
+const routes: RouteRecordRaw[] = [{ path: "", component: HomePage }];
+
+export default routes;
